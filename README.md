d8a240138545da1a0a20fb1ad25e1dd5d24bbad0
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-11-22 22:04:00 +0700

    Progate Study PHP Version 3

---
+++ README.md
@@ -0,0 +1,3 @@
+# Progate Study PHP
+
+<h2>Progate Study PHP Version 3</h2>
---
+++ page9/food.php
@@ -0,0 +1,26 @@
+<?php 
+require_once('menu.php');
+
+class Food extends Menu {
+  // Deklarasikan property private bernama $spiciness
+  private $spiciness;
+  
+  // Override method __construct class induk
+  public function __construct($name, $price, $image, $spiciness) {
+    // $this->name = $name;
+    // $this->price = $price;
+    // $this->image = $image;
+    // $this->spiciness = $spiciniess;
+       parent::__construct($name, $price, $image);
+       $this->spiciness = $spiciness;
+  }
+  // Definisikan method getSpiciness 
+//   $spiciness->getSpiciness;
+//   } 
+// }
+    public function getSpiciness() {            
+     return $this->spiciness;            
+     }            
+   }            
+
+?>
